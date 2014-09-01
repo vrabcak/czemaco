7e6632ec5af9586baf2340792bb422625063f6cb
vrabcak <vrabcak@gmail.com>
2014-09-01 22:38:14 +0200

    reformatted source to make jslint happy

--- js/czemaco.js
+++ js/czemaco.js
@@ -1,17 +1,20 @@
+/*jslint browser: true, sloppy: true */
+/*global L: false, SMap: false, JAK: false  */
 
 /** small helper singleton object
  *  displays log output under map
  *
  */
-var logger = (function (){
+var logger = (function () {
+    'use strict';
     var outel = document.getElementById("debug");
-    outel.innerHTML="logger output";
+    outel.innerHTML = "logger output";
     return {
-        log: function(str){
-                outel.innerHTML=str.toString();
-            }
+        log: function (str) {
+            outel.innerHTML = str.toString();
+        }
     };
-})();
+}());
 
 
 var osmLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -55,7 +58,7 @@ var waymarkedTrailsHikingLayer = L.tileLayer('http://tile.waymarkedtrails.org/hi
 
 
 var llmap = L.map('llmap', {
-    center: [50.0648527,14.518207],
+    center: [50.0648527, 14.518207],
     zoom: 10,
     layers: [osmLayer]
 });
@@ -65,12 +68,11 @@ L.control.layers({
     "Openstreetmap": osmLayer,
     "Prahou na kole": PNKLayer,
     "MTB Mapa": MTBMapLayer
-},
-{
+}, {
     "Waymarked Trails Cycling": waymarkedTrailsCyclingLayer,
     "Waymarked Trails Hiking": waymarkedTrailsHikingLayer
-},
-{collapsed:true
+}, {
+    collapsed: true
 }).addTo(llmap);
 
 L.control.scale({
@@ -83,7 +85,7 @@ L.control.scale({
 
 /************** mapy.cz *****************/
 
-var stred = SMap.Coords.fromWGS84(14.518207,50.0648527);
+var stred = SMap.Coords.fromWGS84(14.518207, 50.0648527);
 var mapa = new SMap(JAK.gel("mapycz"), stred, 8);
 
 
@@ -95,7 +97,7 @@ var markerLayer = new SMap.Layer.Marker();
 mapa.addLayer(markerLayer);
 markerLayer.enable();
 
-var marker = new SMap.Marker(SMap.Coords.fromWGS84(0,0), "myMarker",{});
+var marker = new SMap.Marker(SMap.Coords.fromWGS84(0, 0), "myMarker", {});
 markerLayer.addMarker(marker);
 
 logger.log("All up and running");
@@ -106,28 +108,31 @@ logger.log("All up and running");
  * @param {MouseEvent} e
  * @returns {undefined} nothing
  */
-function moveSMmapFromLlmap(e){
-    var latlon = llmap.getCenter();
-    var zoom = llmap.getZoom();
-    logger.log(latlon.toString()+zoom.toString());
-    mapa.setCenterZoom(SMap.Coords.fromWGS84(latlon.lng,latlon.lat), zoom-2);
+function moveSMmapFromLlmap(e) {
+    'use strict';
+    var latlon = llmap.getCenter(),
+        zoom = llmap.getZoom();
+    logger.log(latlon.toString() + zoom.toString());
+    mapa.setCenterZoom(SMap.Coords.fromWGS84(latlon.lng, latlon.lat), zoom - 2);
 }
 
-llmap.on('move',moveSMmapFromLlmap);
-llmap.on('zoomend',moveSMmapFromLlmap);
+llmap.on('move', moveSMmapFromLlmap);
+llmap.on('zoomend', moveSMmapFromLlmap);
 
 
-function copyMousePositionFromLlmap(e){
+function copyMousePositionFromLlmap(e) {
+    'use strict';
     var latlon = e.latlng;
     logger.log(latlon);
-    
-    marker.setCoords(SMap.Coords.fromWGS84(latlon.lng,latlon.lat));
-    
 
-    
+    marker.setCoords(SMap.Coords.fromWGS84(latlon.lng, latlon.lat));
+
+
+
 }
 
 llmap.on('mousemove', function (e) {
+    'use strict';
     copyMousePositionFromLlmap(e);
 });
 
@@ -137,32 +142,33 @@ llmap.on('mousemove', function (e) {
  * @param {LatLng} latlng
  * @returns {string} html string ingluding <a href> tag
  */
-function makeJOSMlink (latlng){
-    var baseUrl = 'http://127.0.0.1:8111/load_and_zoom';
-    var latOffset = 0.001,lngOffset = 0.002;
-    var left = (latlng.lng-lngOffset).toString();
-    var right = (latlng.lng+lngOffset).toString();
-    var top = (latlng.lat+latOffset).toString();
-    var bottom = (latlng.lat-latOffset).toString();
-
-    var linkUrl = baseUrl + '?left='+left+'&right='+right+'&top='+top+'&bottom='+bottom;
-    return '<a href="' + linkUrl +'" target="blank"> Open in JOSM </a>';
+function makeJOSMlink(latlng) {
+    'use strict';
+    var baseUrl = 'http://127.0.0.1:8111/load_and_zoom',
+        latOffset = 0.001,
+        lngOffset = 0.002,
+        left = (latlng.lng - lngOffset).toString(),
+        right = (latlng.lng + lngOffset).toString(),
+        top = (latlng.lat + latOffset).toString(),
+        bottom = (latlng.lat - latOffset).toString(),
+        linkUrl = baseUrl + '?left=' + left + '&right=' + right + '&top=' + top + '&bottom=' + bottom;
+    return '<a href="' + linkUrl + '" target="blank"> Open in JOSM </a>';
 }
 
-function makeGoogleMapslink (latlng){
-    var lat = latlng.lat.toString();
-    var lng = latlng.lng.toString();
-    var linkUrl = 'http://www.google.com/maps/search/'+lat+','+lng;  
-    return '<a href="' + linkUrl +'" target="blank"> Google maps </a>';
+function makeGoogleMapslink(latlng) {
+    var lat = latlng.lat.toString(),
+        lng = latlng.lng.toString(),
+        linkUrl = 'http://www.google.com/maps/search/' + lat + ',' + lng;
+    return '<a href="' + linkUrl + '" target="blank"> Google maps </a>';
 }
 
-function makeSeznamMaplink (latlng){
-    var lat = latlng.lat.toString();
-    var lng = latlng.lng.toString();
-    var linkUrl = 'http://mapy.cz/cykloturisticka?x='+lng+'&y='+lat+'&z=15&l=0&pano=1';
-    return '<a href="' + linkUrl +'" target="blank"> Mapy.cz </a>';
- }
- 
+function makeSeznamMaplink(latlng) {
+    var lat = latlng.lat.toString(),
+        lng = latlng.lng.toString(),
+        linkUrl = 'http://mapy.cz/cykloturisticka?x=' + lng + '&y=' + lat + '&z=15&l=0&pano=1';
+    return '<a href="' + linkUrl + '" target="blank"> Mapy.cz </a>';
+}
+
 var contextMenu = {
     contextMenuEl: document.getElementById('contextmenu'),
     addLink: function (htmlString) {
@@ -171,35 +177,35 @@ var contextMenu = {
     empty: function () {
         this.contextMenuEl.innerHTML = " ";
     },
-    show: function (left,top,menuitems) {
+    show: function (left, top, menuitems) {
+        var i;
+        
         this.empty();
-        for (var i=0; i<menuitems.length; i+=1 ){
+        for (i = 0; i < menuitems.length; i += 1) {
             this.addLink(menuitems[i]);
         }
         this.contextMenuEl.style.visibility = 'visible';
         this.contextMenuEl.style.left = left + 'px';
         this.contextMenuEl.style.top = top + 'px';
     },
-    hide: function(){
+    hide: function () {
         this.contextMenuEl.style.visibility = 'hidden';
     }
 };
 
 //contextMenuEl = document.getElementById('contextmenu');
 
-llmap.on('contextmenu', function (e){
-    contextMenu.show(e.containerPoint.x, e.containerPoint.y, 
-                     [  makeJOSMlink(e.latlng),
+llmap.on('contextmenu', function (e) {
+    contextMenu.show(e.containerPoint.x, e.containerPoint.y, [makeJOSMlink(e.latlng),
                         makeGoogleMapslink(e.latlng),
                         makeSeznamMaplink(e.latlng)
                     ]);
 });
 
-llmap.on('click', function (e){
-  contextMenu.hide();
+llmap.on('click', function (e) {
+    contextMenu.hide();
 });
 
-llmap.on('drag', function (e){
-  contextMenu.hide();
+llmap.on('drag', function (e) {
+    contextMenu.hide();
 });
-
