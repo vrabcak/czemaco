2ee84d246b9eda4e9d57000aa9b56e4e703928b1
vrabcak <vrabcak@gmail.com>
2015-12-30 13:07:43 +0100

    script for automatic publishing to github pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function (grunt) {
                 },
 
                 src: 'czemaco.html',
-                dest: 'build/index.html'
+                dest: 'dist/index.html'
 
             }
 
